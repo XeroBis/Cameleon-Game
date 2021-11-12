78da864b51f23a9969653459ec178beecd0d2db9
Bis <43960612+XeroBis@users.noreply.github.com>
2021-11-12 17:06:47 +0100

    cleared main and made some comment on game.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -9,228 +9,222 @@ import java.util.Scanner;
 import model.Model;
 
 public class Game {
-    private boolean isBrave;
-    private boolean isGloutonne;
-    public static int blanc = 0, bleu = 1, rouge = 2;
+	private boolean isBrave;
+	private boolean isGloutonne;
+	public static int blanc = 0, bleu = 1, rouge = 2; // si en jvj, bleu = joueur 1 et rouge = joueur 2
 
-    private Model model;
+	private Model model;
 
-    public Game() throws IOException {
-       loadFiles();
-    }
+	public Game() {
+	}
+	/*
+	 * fonction lançant une partie du jeu.
+	 */
+	public void play() {
+		this.loadFiles();
+		this.parametreOfGame();
+		this.playJvJ();
+	}
+	
+	/*
+	 * fonction permettant le chargement d'un fichier txt afin de repmplir le plateau, on peut aussi décider de ne pas 
+	 * joindre de fichier et nous devrons dans ce cas donner un k
+	 */
+	private void loadFiles() {
+		Scanner scan = new Scanner(System.in);
 
-    
-    private void loadFiles() throws IOException {
-    	Scanner scan = new Scanner(System.in);
-    	
-    	boolean choose = true;
+		boolean choose = true;
 		boolean preload = false;
-		
-		while(choose)
-		{
+
+		while (choose) {
 			System.out.print("Voulez vous charger un plateau (y/n) : ");
 			String resp = scan.next();
 			System.out.println();
-			
-			switch (resp)
-			{
-			case "y" :
+
+			switch (resp) {
+			case "y":
 				preload = true;
 				choose = false;
-			break;
-			case "n" :
+				break;
+			case "n":
 				preload = false;
 				choose = false;
-			break;
-			default :
+				break;
+			default:
 				System.out.println("Reponse non valide, seuls 'y' et 'n' sont valides");
-			break;
+				break;
 			}
 		}
-		
-		if(preload) { // donc on load un fichier
-			
+
+		if (preload) { // donc on load un fichier
+
 			System.out.print("entrez un nom de fichier : ");
 			String name = scan.next();
-			
-			System.out.println(System.getProperty("user.dir"));
-			readTextFile(name);
-			
-			
+			try {
+				readTextFile(name);
+			} catch (IOException e) {
+				System.out.println("Fichier n'existe pas");
+				e.printStackTrace();
+			}
+
 		} else { // on load rien
 			System.out.print("entrez la taille du plateau k (dans la formule 3 * 2 ^ k) : ");
 			String k = scan.next();
-			
-			this.model.setNewK(Integer.parseInt(k));
+
+			this.model = new Model(Integer.parseInt(k));
 		}
-		
-		
-		
-		
-    }
-    
-    
-    public void play() throws IOException {
-    	this.loadFiles();
-    }
-    
-    
-    
-    private void parametreOfGame(int n){
-        System.out.println("Le jeu commence !");
-        System.out.println("Vous pouvez jouer en version Brave ou Téméraire que choisissez vous? (0/1)");
-        Scanner scan = new Scanner(System.in);
-        int version = -1;
-        version = scan.nextInt();
-        while(version != 0 && version != 1) {
-            System.out.print("Veuillez entrez la valeur 0 pour la version Brave et 1 pour la version Téméraire.");
-            version = scan.nextInt();
-        }
-        if (version == 0){
-            System.out.println("Vous avez donc choisi la version Brave !");
-            this.setBrave(true);
-            this.setModel(new Model(n));
-        } else {
-            System.out.println("Vous avez donc choisi la version Téméraire !");
-            this.setBrave(false);
-            this.setModel(new Model(n));
-        }
-        /*
-        System.out.println("L'IA peut jouer de deux façon différentes, Gloutonne ou Inteligente, que choisissez vous? (0/1)");
-        int versionIA = -1;
-        while(versionIA != 0 && versionIA != 1) {
-            System.out.print("Veuillez entrez la valeur 0 pour la version Gloutonne et 1 pour la version Intelligente.");
-            versionIA = scan.nextInt();
-        }
-        if (versionIA== 0){
-            System.out.println("Vous avez donc choisi la version Gloutonne !");
-            this.setGloutonne(true);
-        } else {
-            System.out.println("Vous avez donc choisi la version Intelligente !");
-            this.setGloutonne(false);
-        }
-        */
-        
-        //scan.close();
-    }
 
-    private void to_string(){
-        this.model.afficher();
-    }
-    
-    
+	}
 
-    public void playJvJ() {
-        int playing = 0;
-        Scanner scan = new Scanner(System.in);
-        while(playing==0){
-            this.to_string();
+	/*
+	 * fonction nous permettant de parametrer tout les parametres pour une partie
+	 * càd, la version du jeu, la version de l'ia
+	 */
+	private void parametreOfGame() {
+		System.out.println("Le jeu commence !");
+		System.out.println("Vous pouvez jouer en version Brave ou Téméraire que choisissez vous? (0/1)");
+		Scanner scan = new Scanner(System.in);
+		int version = -1;
+		version = scan.nextInt();
+		while (version != 0 && version != 1) {
+			System.out.print("Veuillez entrez la valeur 0 pour la version Brave et 1 pour la version Téméraire.");
+			version = scan.nextInt();
+		}
+		if (version == 0) {
+			System.out.println("Vous avez donc choisi la version Brave !");
+			this.setBrave(true);
+		} else {
+			System.out.println("Vous avez donc choisi la version Téméraire !");
+			this.setBrave(false);
+		}
+		/*
+		 * System.out.
+		 * println("L'IA peut jouer de deux façon différentes, Gloutonne ou Inteligente, que choisissez vous? (0/1)"
+		 * ); int versionIA = -1; while(versionIA != 0 && versionIA != 1) { System.out.
+		 * print("Veuillez entrez la valeur 0 pour la version Gloutonne et 1 pour la version Intelligente."
+		 * ); versionIA = scan.nextInt(); } if (versionIA== 0){
+		 * System.out.println("Vous avez donc choisi la version Gloutonne !");
+		 * this.setGloutonne(true); } else {
+		 * System.out.println("Vous avez donc choisi la version Intelligente !");
+		 * this.setGloutonne(false); }
+		 */
+	}
 
-            System.out.println("JOUEUR 1 : ");
-            int[] coord = chooseCoordinate();
-            boolean play = this.model.colorationBrave(coord[1], coord[0], rouge);
-            while(!play){
-                System.out.println("La case que vous avez selectionnée est déjà colorié, veuillez choisir une autre case.");
-                coord = chooseCoordinate();
-                play = this.model.colorationBrave(coord[1],coord[0], rouge);
-            }
-            
-            this.to_string();
-            System.out.println("JOUEUR 2 : ");
-            coord = chooseCoordinate();
-            play = this.model.colorationBrave(coord[1], coord[0], bleu);
-            while(!play){
-                System.out.println("La case que vous avez selectionnée est déjà colorié, veuillez choisir une autre case.");
-                coord = chooseCoordinate();
-                play = this.model.colorationBrave(coord[1],coord[0], bleu);
-            }
-            
-            
+	/*
+	 * Dessine sur le terminal le plateau de jeu
+	 */
+	private void to_string() {
+		this.model.afficher();
+	}
+	
+	/*
+	 * fonction lançant une partie Joueur versus Joueur
+	 */
+	public void playJvJ() {
+		int playing = 0;
+		Scanner scan = new Scanner(System.in);
+		while (playing == 0) {
+			this.to_string();
 
-        }
-        scan.close();
+			System.out.println("JOUEUR 1 : ");
+			int[] coord = chooseCoordinate();
+			boolean play = this.model.colorationBrave(coord[1], coord[0], bleu);
+			while (!play) {
+				System.out.println(
+						"La case que vous avez selectionnée est déjà colorié, veuillez choisir une autre case.");
+				coord = chooseCoordinate();
+				play = this.model.colorationBrave(coord[1], coord[0], bleu);
+			}
 
-    }
+			this.to_string();
+			System.out.println("JOUEUR 2 : ");
+			coord = chooseCoordinate();
+			play = this.model.colorationBrave(coord[1], coord[0], rouge);
+			while (!play) {
+				System.out.println(
+						"La case que vous avez selectionnée est déjà colorié, veuillez choisir une autre case.");
+				coord = chooseCoordinate();
+				play = this.model.colorationBrave(coord[1], coord[0], rouge);
+			}
 
-    void playIA(){
+		}
+		scan.close();
 
-    }
+	}
 
-    private int[] chooseCoordinate(){
-        int[] res = new int[2];
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Veuillez entrez un numero de ligne :");
-        res[0] = scan.nextInt();
-        while(res[0] > model.getSize()-1 || res[0] < 0){
-            System.out.println("Veuillez indiquez un numéro de ligne compris entre 0 et" + model.getSize()+ " exclus.");
-            res[0] = scan.nextInt();
-        }
-        System.out.print("Veuillez entrez un numero de colonne :");
-        res[1] = scan.nextInt();
-        while(res[1] > model.getSize()-1 || res[1] < 0){
-            System.out.println("Veuillez indiquez un numéro de colonne compris entre 0 et" + model.getSize()+ " exclus.");
-            res[1] = scan.nextInt();
-        }
-        System.out.println("Vous avez choisi le point en ("+res[0] +","+res[1]+")");
-        return res;
-    }
+	void playIA() {
 
-    private void setBrave(boolean brave) {
-        isBrave = brave;
-    }
+	}
+	/*
+	 * fonction permettant le choix, par l'utilisateur des coordonnées de son prochain coup
+	 * @return int[], le tableau contenant les 2 coordonnées choisie du joueur
+	 */
+	private int[] chooseCoordinate() {
+		int[] res = new int[2];
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Veuillez entrez un numero de colonne :");
+		res[0] = scan.nextInt();
+		while (res[0] > model.getSize() - 1 || res[0] < 0) {
+			System.out
+					.println("Veuillez indiquez un numéro de colonne compris entre 0 et" + model.getSize() + " exclus.");
+			res[0] = scan.nextInt();
+		}
+		System.out.print("Veuillez entrez un numero de colonne :");
+		res[1] = scan.nextInt();
+		while (res[1] > model.getSize() - 1 || res[1] < 0) {
+			System.out.println(
+					"Veuillez indiquez un numéro de colonne ligne entre 0 et" + model.getSize() + " exclus.");
+			res[1] = scan.nextInt();
+		}
+		System.out.println("Vous avez choisi le point en (" + res[0] + "," + res[1] + ")");
+		return res;
+	}
 
-    private Model getModel(){ return this.model;}
-    private void setModel(Model model){
-
-        this.model = model;
-    }
-    
-    private void setGloutonne(boolean g) {
-    	this.isGloutonne = g;
-    }
-    
-    public void readTextFile(String filename) throws IOException
-	{
+	/*
+	 * Set le boolean Brave
+	 * @param brave, si la version du jeu est brave
+	 */
+	private void setBrave(boolean brave) {
+		isBrave = brave;
+	}
+	/*
+	 * fonction permettant de lire un fichier txt et de remplir notre plateau avec les données contenu dans ce fichier
+	 */
+	public void readTextFile(String filename) throws IOException {
 		String dir = System.getProperty("user.dir");
-		
+
 		File file = new File(dir + "\\" + filename);
 		BufferedReader reader = new BufferedReader(new FileReader(file));
-		
+
 		String str;
 		str = reader.readLine();
-		
-		
+
 		int a = Integer.parseInt(str);
 		int b = 0;
-		while (a != 3)
-		{
+		while (a != 3) {
 			a = a / 2;
 			b++;
 		}
-		
-		this.model.setNewK(b);
+
+		this.model = new Model(b);
 		int ligne = 0;
-		
-		while ((str = reader.readLine()) != null)
-		{
-			for(int col = 0; col < str.length(); col++)
-			{
-				switch(str.charAt(col))
-				{
+
+		while ((str = reader.readLine()) != null) {
+			for (int col = 0; col < str.length(); col++) {
+				switch (str.charAt(col)) {
 				case 'R':
 					this.model.coloration(ligne, col, 2);
-				break;
+					break;
 				case 'B':
-					this.model.coloration(ligne,  col,  1);
-				break;
+					this.model.coloration(ligne, col, 1);
+					break;
 				default:
-				break;
+					break;
 				}
 			}
-		ligne++;
+			ligne++;
 		}
+		reader.close();
 	}
-    
-    
-    
+
 }
--- src/model/QuadTree.java
+++ src/model/QuadTree.java
@@ -1,130 +1,120 @@
 package model;
 
-public class QuadTree 
-{
+public class QuadTree {
 	private Integer value;
 	private Point point;
 	private QuadTree qt0, qt1, qt2, qt3;
-	
-	public static int blanc = 0,
-	          		  bleu  = 1,
-	          		  rouge = 2;
-	
-	public QuadTree()
-	{
+	public static int blanc = 0, bleu = 1, rouge = 2;
+
+	/*
+	 * Constructeur de QuadTree
+	 */
+	public QuadTree() {
 		this.value = null;
 		this.point = null;
-		
 		this.qt0 = null;
 		this.qt1 = null;
 		this.qt2 = null;
 		this.qt3 = null;
 	}
-	
-	public QuadTree(Integer value, Point point, QuadTree qt0, QuadTree qt1, QuadTree qt2, QuadTree qt3)
-	{
+
+	/*
+	 * Constructeur de QuadTree
+	 * 
+	 * @param value, la couleur du QuadTree
+	 * 
+	 * @param point, le point en haut à gauche de la zone
+	 * 
+	 * @param qt0, qt1, qt2 et qt3, les 4 sous-zones.
+	 */
+	public QuadTree(Integer value, Point point, QuadTree qt0, QuadTree qt1, QuadTree qt2, QuadTree qt3) {
 		this.value = value;
 		this.point = point;
-		
 		this.qt0 = qt0;
 		this.qt1 = qt1;
 		this.qt2 = qt2;
 		this.qt3 = qt3;
 	}
-	
-	public void setQt(int index, QuadTree newQuad)
-	{
-		switch(index)
-		{
+
+	public void setQt(int index, QuadTree newQuad) {
+		switch (index) {
 		case 0:
 			this.qt0 = newQuad;
 			break;
-			
+
 		case 1:
 			this.qt1 = newQuad;
 			break;
-			
+
 		case 2:
 			this.qt2 = newQuad;
 			break;
-			
+
 		case 3:
 			this.qt3 = newQuad;
 			break;
-			
+
 		default:
 			System.out.println("Pas le bon choix d'index, doit etre compris entre 0 et 3 (inclus)");
 		}
 	}
-	
-	public void setValue (Integer newValue)
-	{
+
+	public void setValue(Integer newValue) {
 		this.value = newValue;
 	}
-	
-	public void setPoint (Point newPoint)
-	{
+
+	public void setPoint(Point newPoint) {
 		this.point = newPoint;
 	}
-	
-	public QuadTree getQt(int index)
-	{
+
+	public QuadTree getQt(int index) {
 		QuadTree ret = null;
-		
-		switch(index)
-		{
+
+		switch (index) {
 		case 0:
 			ret = this.qt0;
 			break;
-			
+
 		case 1:
 			ret = this.qt1;
 			break;
-			
+
 		case 2:
 			ret = this.qt2;
 			break;
-			
+
 		case 3:
 			ret = this.qt3;
 			break;
-			
+
 		default:
 			System.out.println("Pas le bon choix d'index, doit etre compris entre 0 et 3 (inclus)");
 		}
-		return ret ;
+		return ret;
 	}
-	
-	public int getValue()
-	{
+
+	public int getValue() {
 		return this.value;
 	}
-	
-	public Point getPoint()
-	{
+
+	public Point getPoint() {
 		return this.point;
 	}
-	
-	public void printValuePoint()
-	{
-		if(this.point != null && this.value != null)
-		{
+
+	public void printValuePoint() {
+		if (this.point != null && this.value != null) {
 			System.out.println(this.value + ";" + this.point.getx() + ";" + this.point.gety());
 		}
-		if(this.qt0 != null )
-		{
+		if (this.qt0 != null) {
 			qt0.printValuePoint();
 		}
-		if(this.qt1 != null )
-		{
+		if (this.qt1 != null) {
 			qt1.printValuePoint();
 		}
-		if(this.qt2 != null )
-		{
+		if (this.qt2 != null) {
 			qt2.printValuePoint();
 		}
-		if(this.qt3 != null )
-		{
+		if (this.qt3 != null) {
 			qt3.printValuePoint();
 		}
 	}
