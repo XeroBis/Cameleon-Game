4a56385d8ad5afe6589fac15d5093bebd231a4ec
Bis <43960612+XeroBis@users.noreply.github.com>
2021-12-01 11:56:57 +0100

    fixed main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,38 +1,12 @@
 package main;
 
 import game.Game;
-import view.View;
 
-import javax.swing.SwingUtilities;
 
 public class Main
 {
 	public static void main(String[] args) {
-		//Game g = new Game();
-		//g.play(true); // on lance le jeu avec cette commmande
-		//System.out.print(System.getProperty("user.dir"));
-		
-		
-		
-		java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-            	new View("JEU", 1000, 1000);
-            }
-        });
-		
-		/*
-		SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                view.setVisible(true);
-            }
-        });
-        */
-		
-		
+		Game g = new Game();
+		g.play(true); // on lance le jeu avec cette commmande
 	}
-	/*
-	public static void createAndShowGUI() throws Exception {
-        new View("JEU", 1000, 1000);
-    }
-    */
 }
